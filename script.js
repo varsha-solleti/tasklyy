730d2cafced16e34766c3a4bb7ca471d95b8ef46
varsha-solleti <solletivarsha@gmail.com>
2026-08-18 22:27:20 +0530

    Fix empty task message

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
 const taskInput = document.getElementById("taskInput");
 const addTaskBtn = document.getElementById("addTaskBtn");
 const taskList = document.getElementById("taskList");
+const emptyMessage = document.getElementById("emptyMessage");
 
+
+// ADD TASK
 addTaskBtn.addEventListener("click", function () {
+
     const taskText = taskInput.value.trim();
 
     if (taskText === "") {
         return;
     }
+
+    createTask(taskText);
+
+    taskInput.value = "";
+
+    updateProgress();
+    saveTasks();
+});
+
+
+// CREATE TASK
+function createTask(taskText) {
 
     const task = document.createElement("div");
     task.classList.add("task");
@@ -19,24 +35,45 @@ addTaskBtn.addEventListener("click", function () {
     `;
 
     taskList.appendChild(task);
+}
 
-    taskInput.value = "";
-});
 
+// DELETE TASK
 taskList.addEventListener("click", function (event) {
+
     if (event.target.classList.contains("delete-btn")) {
+
         event.target.parentElement.remove();
+
         updateProgress();
+        saveTasks();
     }
 });
 
+
+// CHECKBOX
 taskList.addEventListener("change", function (event) {
+
     if (event.target.type === "checkbox") {
+
         updateProgress();
+        saveTasks();
     }
 });
 
+
+// ENTER KEY
+taskInput.addEventListener("keydown", function (event) {
+
+    if (event.key === "Enter") {
+        addTaskBtn.click();
+    }
+});
+
+
+// UPDATE PROGRESS
 function updateProgress() {
+
     const tasks = taskList.querySelectorAll(".task");
     const completed = taskList.querySelectorAll(".task input:checked");
 
@@ -44,7 +81,11 @@ function updateProgress() {
     const completedCount = completed.length;
 
     const remainingCount = totalTasks - completedCount;
-    const percentage = totalTasks === 0 ? 0 : (completedCount / totalTasks) * 100;
+
+    const percentage =
+        totalTasks === 0
+        ? 0
+        : (completedCount / totalTasks) * 100;
 
     document.getElementById("remainingTasks").textContent =
         `${remainingCount} tasks remaining`;
@@ -54,10 +95,69 @@ function updateProgress() {
 
     document.querySelector(".progress").style.width =
         `${percentage}%`;
+   
+        
+    
+updateEmptyMessage();
 }
 
-taskInput.addEventListener("keydown", function (event) {
-    if (event.key === "Enter") {
-        addTaskBtn.click();
+
+// SAVE TASKS
+function saveTasks() {
+
+    const tasks = [];
+
+    document.querySelectorAll(".task").forEach(function (task) {
+
+        const text = task.querySelector("label").textContent;
+
+        const completed =
+            task.querySelector("input").checked;
+
+        tasks.push({
+            text: text,
+            completed: completed
+        });
+
+    });
+
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+
+// LOAD TASKS
+function loadTasks() {
+
+    const savedTasks =
+        JSON.parse(localStorage.getItem("tasks")) || [];
+
+    savedTasks.forEach(function (task) {
+
+        createTask(task.text);
+
+        const allTasks =
+            document.querySelectorAll(".task");
+
+        const lastTask =
+            allTasks[allTasks.length - 1];
+
+        lastTask.querySelector("input").checked =
+            task.completed;
+    });
+
+    updateProgress();
+}
+
+function updateEmptyMessage() {
+    const tasks = taskList.querySelectorAll(".task");
+
+    if (tasks.length === 0) {
+        emptyMessage.style.display = "block";
+    } else {
+        emptyMessage.style.display = "none";
     }
-});
+}
+
+
+// START APP
+loadTasks();
